c8756b8859b84813c36b973490dca97ec7fa0da8
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-21 20:29:23 +0530

    style update for visit

--- src/components/visits.jsx
+++ src/components/visits.jsx
@@ -18,7 +18,7 @@ const Visits = () => {
   return (
     <div className="w-min m-auto rounded-3xl p-px bg-gradient-to-b from-blue-300 to-pink-300 dark:from-blue-800 dark:to-purple-800 ">
       <div className="rounded-[calc(1.5rem-1px)] p-1 py-0 bg-white dark:bg-gray-900">
-        <span className="text-nowrap text-muted-foreground font-light text-sm">
+        <span className="text-nowrap text-muted-foreground font-light text-sm px-1">
           {count.N} Visits
         </span>
       </div>
