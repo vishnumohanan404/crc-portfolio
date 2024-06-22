19d74eeb19d275732dd54d16f89e0f72ea6617b4
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-22 19:17:23 +0530

    visit refetch issue test

--- src/components/visits.jsx
+++ src/components/visits.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 
 const Visits = () => {
   const [count, setCount] = useState("");
-  console.log("hellop");
+
   useEffect(() => {
-    console.log("Useeffect ran");
-    if (!window.sessionStorage.getItem("visited")) {
-      window.sessionStorage.setItem("visited", true);
-      const fetchUpdate = async () => {
+    const sessionData = sessionStorage.getItem("count");
+
+    // alert("Useeffect ran", sessionData);
+    if (sessionData) {
+      setCount(JSON.parse(sessionData));
+    } else {
+      (async () => {
         const updateCount = await fetch(
           import.meta.env.VITE_API_GATEWAY_ENDPOINT + "/visitors",
           {
@@ -15,27 +18,15 @@ const Visits = () => {
           }
         );
         const countObj = await new Response(updateCount.body).json();
+        sessionStorage.setItem("count", JSON.stringify(countObj.count));
         setCount(countObj.count);
-      };
-      fetchUpdate();
-    } else {
-      const fetchCount = async () => {
-        const updateCount = await fetch(
-          import.meta.env.VITE_API_GATEWAY_ENDPOINT + "/visitors",
-          {
-            method: "GET",
-          }
-        );
-        const countObj = await new Response(updateCount.body).json();
-        setCount(countObj.count.N);
-      };
-      fetchCount();
+      })();
     }
   }, []);
   return (
     <div className="w-min m-auto rounded-3xl p-px bg-gradient-to-b from-blue-300 to-pink-300 dark:from-blue-800 dark:to-purple-800 ">
       <div className="rounded-[calc(1.5rem-1px)] p-1 py-0 bg-white dark:bg-gray-900">
-        <span className="text-nowrap text-muted-foreground font-light text-sm px-1">
+        <span className="text-nowrap text-muted-foreground font-light text-sm px-1 whitespace-nowrap">
           {count} Visits
         </span>
       </div>
