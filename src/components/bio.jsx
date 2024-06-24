b685f69c11f102b20d8c66faf81c00e3d533c083
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-06-24 19:58:17 +0530

    responsiveness fixed

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const Bio = () => {
   return (
-    <div className="bg-white border-zinc-200 border w-[80%] mx-auto p-6 py-3 rounded-full flex justify-between items-center">
+    <div className="bg-white border-zinc-200 border md:w-[80%] w-[100%] mx-auto p-6 py-3 rounded-full flex justify-between items-center">
       <div>
         <p className="font-sans">Vishnu Mohan</p>
-        <p className="font-sans font-light text-sm text-gray-500">
-          Software Engineer, DevOps
+        <p className="font-sans font-light text-sm text-gray-600">
+          DevOps Engineer |{" "}
+          <span className="font-sans text-xs text-gray-500 ">
+            Ex-Rhombuz.io
+          </span>
         </p>
       </div>
-      <div className="flex-col gap-1 items-center hidden md:flex">
+      <div className="flex-col gap-1 items-center flex">
         <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
           OPEN FOR WORK
         </span>
